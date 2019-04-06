15429dd6039acf78cc0b94fe37efd27739be8188
Robin La Rondelle <ra.larondelle@student.avans.nl>
2019-04-06 16:39:39 +0200

    Added projects databinding

--- Portfolio/src/app/main/main.component.ts
+++ Portfolio/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  private projects: Project[]
 
-  ngOnInit() {
+  constructor() { 
+    this.projects = []
   }
 
+  ngOnInit() {
+    const studentHouse: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "StudentHouse"
+    }
+
+    const ticketstore: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "ticketstore"
+    }
+
+    const brwHvtSvr: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "brwHvtSvr"
+    }
+
+    const brwHvtPage: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "brwHvtPage"
+    }
+
+    const kramse: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "kramse"
+    }
+
+    const algoritmenDatastructures: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "algoritmenDatastructures"
+    }
+
+    const cineApp: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "cineApp"
+    }
+
+    const handPickedAgencies: Project = {
+      imagelink: "",
+      githublink: "",
+      name: "handPickedAgencies"
+    }
+
+    this.projects.push(
+      studentHouse,
+      ticketstore,
+      brwHvtSvr,
+      brwHvtPage,
+      kramse,
+      algoritmenDatastructures, 
+      cineApp, 
+      handPickedAgencies
+    )
+  }
 }
---
+++ Portfolio/src/app/models/project.model.ts
@@ -0,0 +1,5 @@
+export class Project {
+  name: String
+  githublink: String
+  imagelink: String
+}
